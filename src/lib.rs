ea65b5cb24c5c33669bebb2bfcef21565a791359
Pierre Abbat <phma@bezitopo.org>
2023-07-11 07:08:05 +0000

    Define mix in Rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+pub fn mix(a: &mut u8, b: &mut u8, c: &mut u8) {
+  let mask:u8=(*a|*b|*c)-(*a&*b&*c);
+  *a=*a^mask;
+  *b=*b^mask;
+  *c=*c^mask;
+}
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
