f04d22f47b314b48dfc54cc198c3674992985229
Pierre Abbat <phma@bezitopo.org>
2023-07-26 19:35:23 +0000

    Rewrite mix to avoid obscure code that pacifies the borrow checker

--- src/mix3.rs
+++ src/mix3.rs
@@ -24,11 +24,9 @@ use num_prime::nt_funcs::factorize64;
 use mod_exp::*;
 use std::mem::replace;
 
-pub fn mix(a: &mut u8, b: &mut u8, c: &mut u8) {
-  let mask:u8=(*a|*b|*c)-(*a&*b&*c);
-  *a=*a^mask;
-  *b=*b^mask;
-  *c=*c^mask;
+pub fn mix(a: u8, b: u8, c: u8) -> (u8,u8,u8) {
+  let mask:u8=(a|b|c)-(a&b&c);
+  (a^mask,b^mask,c^mask)
 }
 
 pub fn fibo_pair(n: BigUint) -> (BigUint,BigUint) {
@@ -70,13 +68,10 @@ pub fn mix3parts(buf: &mut[u8], len: usize, rprime: usize) {
   let mut b=2*len-1;
   let mut c=2*len;
   while a<b {
-    // This amounts to mix(buf[a],buf[b],buf[c]).
-    // It has to be written in this convoluted way
-    // to keep the borrow checker happy.
-    let (d,rest)=buf[a..].split_first_mut().unwrap();
-    let (e,rest)=rest[b-a-1..].split_first_mut().unwrap();
-    let (f,_)=rest[c-b-1..].split_first_mut().unwrap();
-    mix(d,e,f);
+    let (d,e,f)=mix(buf[a],buf[b],buf[c]);
+    buf[a]=d;
+    buf[b]=e;
+    buf[c]=f;
     a=a+1;
     b=b-1;
     c=c+rprime;
