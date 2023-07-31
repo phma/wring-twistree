0238bfc4c7b8e84fad998718f372820253d91a21
Pierre Abbat <phma@bezitopo.org>
2023-07-31 09:49:53 +0000

    Make a linear s-box to use as a dummy

--- src/Cryptography/Wring.hs
+++ src/Cryptography/Wring.hs
@@ -1,13 +1,19 @@
 module Cryptography.Wring
   ( Wring (..)
+  , linearSbox
   ) where
 
 import Cryptography.WringTwistree.Mix3
 import Cryptography.WringTwistree.RotBitcount
 import Data.Word
+import Data.Bits
 import Data.Array.Unboxed
 
 data Wring = Wring
   { sbox    :: UArray (Word8,Word8) Word8
   , invSbox :: UArray (Word8,Word8) Word8
   }
+
+linearSbox = array ((0,0),(2,255))
+  [ ((i,j),xor i j) | i <- [0..2], j <- [0..255] ]
+  :: UArray (Word8,Word8) Word8
