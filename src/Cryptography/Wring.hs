9853033fa8b9cd49a354b539ade2d3c4c71ff3f8
Pierre Abbat <phma@bezitopo.org>
2023-08-02 04:47:53 +0000

    Define encryption and decryption in Haskell

--- src/Cryptography/Wring.hs
+++ src/Cryptography/Wring.hs
@@ -2,6 +2,8 @@ module Cryptography.Wring
   ( Wring (..)
   , linearSbox
   , roundEncrypt
+  , encrypt
+  , decrypt
   ) where
 
 import Cryptography.WringTwistree.Mix3
@@ -9,6 +11,7 @@ import Cryptography.WringTwistree.RotBitcount
 import Data.Word
 import Data.Bits
 import Data.Array.Unboxed
+import Data.Foldable (toList,foldl')
 
 data Wring = Wring
   { sbox    :: UArray (Word8,Word8) Word8
@@ -32,9 +35,23 @@ linearSbox = array ((0,0),(2,255))
  -}
 
 roundEncrypt :: (Ix a,Integral a,Bits a) => 
-  a -> UArray (Word8,Word8) Word8 ->UArray a Word8 -> a -> UArray a Word8
+  a -> UArray (Word8,Word8) Word8 -> UArray a Word8 -> a -> UArray a Word8
 roundEncrypt rprime sbox buf rond = rotBitcount (mix3Parts buf (fromIntegral rprime)) 1
 
 roundDecrypt :: (Ix a,Integral a,Bits a) => 
-  a -> UArray (Word8,Word8) Word8 ->UArray a Word8 -> a -> UArray a Word8
+  a -> UArray (Word8,Word8) Word8 -> UArray a Word8 -> a -> UArray a Word8
 roundDecrypt rprime sbox buf rond = mix3Parts (rotBitcount buf (-1)) (fromIntegral rprime)
+
+encrypt :: (Ix a,Integral a,Bits a) => Wring -> UArray a Word8 -> UArray a Word8
+encrypt wring buf = foldl' (roundEncrypt rprime (sbox wring)) buf rounds
+  where
+    len = fromIntegral $ snd (bounds buf) +1
+    rprime = fromIntegral $ findMaxOrder (len `div` 3)
+    rounds = [0 .. (fromIntegral (nRounds len) -1)]
+
+decrypt :: (Ix a,Integral a,Bits a) => Wring -> UArray a Word8 -> UArray a Word8
+decrypt wring buf = foldl' (roundDecrypt rprime (sbox wring)) buf rounds
+  where
+    len = fromIntegral $ snd (bounds buf) +1
+    rprime = fromIntegral $ findMaxOrder (len `div` 3)
+    rounds = reverse [0 .. (fromIntegral (nRounds len) -1)]
