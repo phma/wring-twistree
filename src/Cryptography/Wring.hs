7e5dd231f80ccdb09830650abc5437b486804f9a
Pierre Abbat <phma@bezitopo.org>
2023-08-02 16:58:48 +0000

    Fix the s-box and use it when encrypting and decrypting

--- src/Cryptography/Wring.hs
+++ src/Cryptography/Wring.hs
@@ -1,5 +1,7 @@
 module Cryptography.Wring
   ( Wring (..)
+  , linearSbox
+  , linearInvSbox
   , linearWring
   , encrypt
   , decrypt
@@ -15,7 +17,7 @@ import Data.Foldable (toList,foldl')
 data Wring = Wring
   { sbox    :: UArray (Word8,Word8) Word8
   , invSbox :: UArray (Word8,Word8) Word8
-  }
+  } deriving Show
 
 nRounds :: Integral a => a -> a
 nRounds len
@@ -23,36 +25,40 @@ nRounds len
   | otherwise = (nRounds (len `div` 3)) +1
 
 linearSbox = array ((0,0),(2,255))
-  [ ((i,j),rotate i (fromIntegral (3*j+1))) | i <- [0..2], j <- [0..255] ]
+  [ ((i,j),rotate j (fromIntegral (3*i+1))) | i <- [0..2], j <- [0..255] ]
   :: UArray (Word8,Word8) Word8
 
 linearInvSbox = array ((0,0),(2,255))
-  [ ((i,j),rotate i (fromIntegral (7-3*j))) | i <- [0..2], j <- [0..255] ]
+  [ ((i,j),rotate j (fromIntegral (7-3*i))) | i <- [0..2], j <- [0..255] ]
   :: UArray (Word8,Word8) Word8
 
 linearWring = Wring linearSbox linearInvSbox
 
 {- A round of encryption consists of four steps:
  - mix3Parts
- - sboxes (omitted)
+ - sboxes
  - rotBitcount
  - add byte index xor round number (omitted)
  -}
 
+cycle3 = 0 : 1 : 2 : cycle3
+
 roundEncrypt :: (Ix a,Integral a,Bits a) => 
   a -> UArray (Word8,Word8) Word8 -> UArray a Word8 -> a -> UArray a Word8
 roundEncrypt rprime sbox buf rond = i4 where
+  bnd = bounds buf
   i1 = mix3Parts buf (fromIntegral rprime)
-  i2 = i1
+  i2 = listArray bnd $ map (sbox !) $ zip (drop (fromIntegral rond) cycle3) (elems i1)
   i3 = rotBitcount i2 1
   i4 = i3
 
 roundDecrypt :: (Ix a,Integral a,Bits a) => 
   a -> UArray (Word8,Word8) Word8 -> UArray a Word8 -> a -> UArray a Word8
 roundDecrypt rprime sbox buf rond = i4 where
+  bnd = bounds buf
   i1 = buf
   i2 = rotBitcount i1 (-1)
-  i3 = i2
+  i3 = listArray bnd $ map (sbox !) $ zip (drop (fromIntegral rond) cycle3) (elems i2)
   i4 = mix3Parts i3 (fromIntegral rprime)
 
 encrypt :: (Ix a,Integral a,Bits a) => Wring -> UArray a Word8 -> UArray a Word8
