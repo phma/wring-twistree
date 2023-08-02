239a93c8129265c42c96488c5eccbd2c40738081
Pierre Abbat <phma@bezitopo.org>
2023-08-02 04:04:51 +0000

    Define the number of rounds and the decryption round in Haskell

--- src/Cryptography/Wring.hs
+++ src/Cryptography/Wring.hs
@@ -15,6 +15,11 @@ data Wring = Wring
   , invSbox :: UArray (Word8,Word8) Word8
   }
 
+nRounds :: Integral a => a -> a
+nRounds len
+  | len < 3 = 1
+  | otherwise = (nRounds (len `div` 3)) +1
+
 linearSbox = array ((0,0),(2,255))
   [ ((i,j),xor i j) | i <- [0..2], j <- [0..255] ]
   :: UArray (Word8,Word8) Word8
@@ -28,4 +33,8 @@ linearSbox = array ((0,0),(2,255))
 
 roundEncrypt :: (Ix a,Integral a,Bits a) => 
   a -> UArray (Word8,Word8) Word8 ->UArray a Word8 -> a -> UArray a Word8
-roundEncrypt rprime sbox buf round = rotBitcount (mix3Parts buf (fromIntegral rprime)) 1
+roundEncrypt rprime sbox buf rond = rotBitcount (mix3Parts buf (fromIntegral rprime)) 1
+
+roundDecrypt :: (Ix a,Integral a,Bits a) => 
+  a -> UArray (Word8,Word8) Word8 ->UArray a Word8 -> a -> UArray a Word8
+roundDecrypt rprime sbox buf rond = mix3Parts (rotBitcount buf (-1)) (fromIntegral rprime)
