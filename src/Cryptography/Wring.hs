5849bbb84d9cffc44f5286640f117683c7a68565
Pierre Abbat <phma@bezitopo.org>
2023-08-03 06:53:45 +0000

    Add the round number xor the byte index in each round

--- src/Cryptography/Wring.hs
+++ src/Cryptography/Wring.hs
@@ -3,6 +3,7 @@ module Cryptography.Wring
   , linearSbox
   , linearInvSbox
   , linearWring
+  , xorn
   , encrypt
   , decrypt
   ) where
@@ -24,6 +25,11 @@ nRounds len
   | len < 3 = 3
   | otherwise = (nRounds (len `div` 3)) +1
 
+xorn :: (Integral a,Bits a) => a -> a
+xorn 0 = 0
+xorn (-1) = error "xorn: negative"
+xorn a = (a .&. 255) `xor` (xorn (a `shiftR` 8))
+
 linearSbox = array ((0,0),(2,255))
   [ ((i,j),rotate j (fromIntegral (3*i+1))) | i <- [0..2], j <- [0..255] ]
   :: UArray (Word8,Word8) Word8
@@ -38,7 +44,7 @@ linearWring = Wring linearSbox linearInvSbox
  - mix3Parts
  - sboxes
  - rotBitcount
- - add byte index xor round number (omitted)
+ - add byte index xor round number
  -}
 
 cycle3 = 0 : 1 : 2 : cycle3
@@ -50,13 +56,15 @@ roundEncrypt rprime sbox buf rond = i4 where
   i1 = mix3Parts buf (fromIntegral rprime)
   i2 = listArray bnd $ map (sbox !) $ zip (drop (fromIntegral rond) cycle3) (elems i1)
   i3 = rotBitcount i2 1
-  i4 = i3
+  i4 = listArray bnd $ zipWith (+) (elems i3)
+       (map (fromIntegral . xorn . (xor rond)) [0..])
 
 roundDecrypt :: (Ix a,Integral a,Bits a) => 
   a -> UArray (Word8,Word8) Word8 -> UArray a Word8 -> a -> UArray a Word8
 roundDecrypt rprime sbox buf rond = i4 where
   bnd = bounds buf
-  i1 = buf
+  i1 = listArray bnd $ zipWith (-) (elems buf)
+       (map (fromIntegral . xorn . (xor rond)) [0..])
   i2 = rotBitcount i1 (-1)
   i3 = listArray bnd $ map (sbox !) $ zip (drop (fromIntegral rond) cycle3) (elems i2)
   i4 = mix3Parts i3 (fromIntegral rprime)
