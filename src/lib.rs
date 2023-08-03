1a07bc5594655b37ba03bef3eb6ece09e6bc3d57
Pierre Abbat <phma@bezitopo.org>
2023-08-03 08:56:31 +0000

    Start wring module and define xorn in Rust

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,20 @@ pub mod mix3;
 
 // Used by both wring and twistree
 pub mod rotbitcount;
+
+pub mod wring {
+
+use crate::mix3::*;
+use crate::rotbitcount::*;
+
+pub fn xorn(n: u64) -> u8 {
+  let mut ret=0u8;
+  let mut i=n;
+  while i>0 {
+    ret ^= i as u8;
+    i = i>>8;
+  }
+  ret
+}
+
+}
