23e1fc97b067cca5af9dd58d5af566ce72e1ab81
Pierre Abbat <phma@bezitopo.org>
2023-08-06 08:59:36 +0000

    Add test cases for permut8

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,33 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+{-# LANGUAGE InstanceSigs #-}
+import Data.Word
+import Test.Tasty
+import Test.Tasty.QuickCheck as QC
+import Test.Tasty.HUnit
+import Cryptography.WringTwistree.Permute
+import qualified Data.Sequence as Seq
+import Data.Foldable (toList)
+
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "Tests" [properties, unitTests]
+
+properties :: TestTree
+properties = testGroup "Properties" [qcProps]
+
+qcProps = testGroup "(checked by QuickCheck)"
+  [
+  ]
+
+wrapPermut8 :: [a] -> Word16 -> [a]
+wrapPermut8 as n = toList $ permut8 (Seq.fromList as) n
+
+unitTests = testGroup "Unit tests"
+  [ testCase "calipers" $
+      (wrapPermut8 "calipers" 0x3c45) @?= "spiracle"
+  , testCase "recounts" $
+      (wrapPermut8 "recounts" 0x595b) @?= "construe"
+  , testCase "thousand" $
+      (wrapPermut8 "thousand" 0x30da) @?= "handouts"
+    -- TODO "repaints" can't be anagrammed to "pantries"
+  ]
