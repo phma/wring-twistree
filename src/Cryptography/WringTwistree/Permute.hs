da6b5dc88638c50eb98be82f2c67041b82efc552
Pierre Abbat <phma@bezitopo.org>
2023-08-07 07:57:32 +0000

    Write two alternatives for the function to permute the s-box

--- src/Cryptography/WringTwistree/Permute.hs
+++ src/Cryptography/WringTwistree/Permute.hs
@@ -1,5 +1,7 @@
 module Cryptography.WringTwistree.Permute
   ( permut8
+  , dealInx
+  , invDealInx
   ) where
 
 {- This module is used in both Wring and Twistree.
@@ -12,6 +14,7 @@ module Cryptography.WringTwistree.Permute
 import Data.Bits
 import Data.Array.Unboxed
 import Data.Word
+import Data.List
 import qualified Data.Sequence as Seq
 import Data.Sequence ((><), (<|), (|>), Seq((:<|)), Seq((:|>)), update)
 
@@ -37,3 +40,22 @@ swapmute ys (x:xs) n = swapmute ys' xs (n+1) where
 
 permut8 :: Seq.Seq a -> Word16 -> Seq.Seq a
 permut8 ys n = swapmute ys (swapOrder n) 1
+
+dealInxA :: Int -> Int
+dealInxA n = ((n+1)*10) `mod` 257-1
+
+invDealInxA :: Int -> Int
+invDealInxA n = ((n+1)*180) `mod` 257-1
+
+-- polynomial 100011101, 3 bit overflow table
+shift3    = listArray (0,7) [0x00,0x1d,0x3a,0x27,0x74,0x69,0x4e,0x53] :: UArray Int Int
+invShift3 = listArray (0,7) [0x00,0xad,0x47,0xea,0x8e,0x23,0xc9,0x64] :: UArray Int Int
+
+dealInxB :: Int -> Int
+dealInxB n = ((n .&. 0x1f) `shiftL` 3) `xor` (shift3 ! ((n .&. 0xe0) `shiftR` 5))
+
+invDealInxB :: Int -> Int
+invDealInxB n = ((n .&. 0xf8) `shiftR` 3) `xor` (invShift3 ! (n .&. 0x07))
+
+dealInx = dealInxB
+invDealInx = invDealInxB
