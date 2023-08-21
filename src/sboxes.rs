bfe65e8b8cbfbd92c821a6e652d169f36096a935
Pierre Abbat <phma@bezitopo.org>
2023-08-21 09:52:06 +0000

    Define sboxes in Rust

--- src/sboxes.rs
+++ src/sboxes.rs
@@ -2,3 +2,16 @@
 // This module is used in both Wring and Twistree.
 // It converts a slice of bytes of arbitrary length, which should not exceed 96,
 // into three s-boxes.
+
+use crate::keyschedule::*;
+use crate::permute::*;
+
+pub fn sboxes(str:&[u8],sbox: &mut [[u8; 256]; 3]) {
+  let mut subkey:[u16; 96]=[0u16; 96];
+  key_schedule(str,&mut subkey);
+  permute256(&mut sbox[0],&mut subkey);
+  reschedule(&mut subkey);
+  permute256(&mut sbox[1],&mut subkey);
+  reschedule(&mut subkey);
+  permute256(&mut sbox[2],&mut subkey);
+}
