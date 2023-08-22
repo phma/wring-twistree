2dc27c42189137729cb8fe61eaca92076dffd916
Pierre Abbat <phma@bezitopo.org>
2023-08-22 06:17:18 +0000

    Parse options

--- app/Main.hs
+++ app/Main.hs
@@ -41,7 +41,48 @@ wrungZeros = encrypt linearWring (listArray (0,255::Int) (replicate 256 0))
 dumpSbox :: UArray (Word8,Word8) Word8 -> IO ()
 dumpSbox sbox = putStr $ block16str $ take 256 (elems sbox)
 
-main :: IO ()
-main = do
+data WtOpt
+  = Infile String
+  | Encrypt
+  | Decrypt
+  | Hash
+  | Key String
+  | Outfile String
+    deriving (Show)
+
+optSpecs :: [OptSpec WtOpt]
+optSpecs =
+  [ optSpec "e" ["encrypt"] (ZeroArg Encrypt)
+  , optSpec "d" ["decrypt"] (ZeroArg Decrypt)
+  , optSpec "H" ["hash"] (ZeroArg Hash)
+  , optSpec "k" ["key"] (OneArg Key)
+  , optSpec "o" ["output"] (OneArg Outfile)
+  ]
+
+help :: String -> String
+help progName = unlines
+  [ progName ++ " - Wring cipher and Twistree hash"
+  , "Usage:"
+  , progName ++ " [options] INPUTFILE ..."
+  , ""
+  , "Options:"
+  , ""
+  , "--encrypt,     -e       Encrypt a file."
+  , "--decrypt,     -d       Decrypt a file."
+  , "--hash,        -H       Hash a file."
+  , "--output FILE, -o FILE  Output results to FILE."
+  , ""
+  , "At most one of -e, -d, and -H may be specified. If -o is not specified,"
+  , "the ciphertext or plaintext overwrites the original file, and the hash"
+  , "is written to standard output."
+  ]
+
+testExtendKey :: IO ()
+testExtendKey = do
   putStr $ block16str16 $ toList $ extendKey $ fromString "roygbiv"
   putStr $ block16str16 $ toList $ extendKey $ fromString "aerate"
+
+main :: IO ()
+main = do
+  parse <- parseCommandLine help optSpecs Infile
+  putStrLn $ show parse
