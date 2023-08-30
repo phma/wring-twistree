545d4268f5085df80ae6115b737921a89ccd7bce
Pierre Abbat <phma@bezitopo.org>
2023-08-30 11:02:20 +0000

    Test mul65537

--- test/Spec.hs
+++ test/Spec.hs
@@ -4,6 +4,7 @@ import Test.Tasty
 import Test.Tasty.QuickCheck as QC
 import Test.Tasty.HUnit
 import Cryptography.WringTwistree.Permute
+import Cryptography.WringTwistree.KeySchedule
 import qualified Data.Sequence as Seq
 import Data.Foldable (toList)
 
@@ -18,9 +19,13 @@ properties = testGroup "Properties" [qcProps]
 wrapPermut8 :: [a] -> Word16 -> [a]
 wrapPermut8 as n = toList $ permut8 (Seq.fromList as) n
 
+seq40504 = 0 : map (mul65537 40504) seq40504
+
 qcProps = testGroup "(checked by QuickCheck)"
   [ QC.testProperty "repaints" $
       \x -> wrapPermut8 "repaints" x /= "pantries"
+  , QC.testProperty "mul65537" $
+      \x y -> mul65537 x y == mul65537 (65535-x) (65535-y)
   ]
 
 unitTests = testGroup "Unit tests"
@@ -30,4 +35,8 @@ unitTests = testGroup "Unit tests"
       (wrapPermut8 "recounts" 0x595b) @?= "construe"
   , testCase "thousand" $
       (wrapPermut8 "thousand" 0x30da) @?= "handouts"
+  , testCase "mul65537 40504" $
+      (length $ takeWhile (/= 0) $ tail seq40504) @?= 65535
+  , testCase "mul65537 65535" $
+      (mul65537 65535 65535) @?= 0
   ]
