69a45f6a7a4a0685f049719156ae462cdfeaa0b9
Pierre Abbat <phma@bezitopo.org>
2023-09-06 08:53:37 +0000

    Add comments about exp(4) and make constants public

--- src/compress.rs
+++ src/compress.rs
@@ -1,5 +1,5 @@
-const BLOCKSIZE: u16=32;
-const TWISTPRIME: u16=37;
+pub const BLOCKSIZE: u16=32;
+pub const TWISTPRIME: u16=37;
 // BLOCKSIZE must be a multiple of 4. Blocks in the process of compression
 // can be any size from BLOCKSIZE to 3*BLOCKSIZE in steps of 4. TWISTPRIME is
 // the smallest prime greater than BLOCKSIZE, which is relatively prime to all
