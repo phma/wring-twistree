5a0ccad360719c608cbaa32268161858beb3e6b8
Pierre Abbat <phma@bezitopo.org>
2023-09-10 08:01:07 +0000

    Write the first three steps of round_compress in Rust

--- src/compress.rs
+++ src/compress.rs
@@ -1,10 +1,11 @@
 extern crate lazy_static;
 use lazy_static::lazy_static;
 use crate::mix3::*;
+use crate::rotbitcount::*;
 
-pub const BLOCKSIZE: u16=32;
-pub const TWISTPRIME: u16=37;
-const SZ: usize=(BLOCKSIZE/2+1) as usize;
+pub const BLOCKSIZE: usize=32;
+pub const TWISTPRIME: isize=37;
+const SZ: usize=BLOCKSIZE/2+1;
 // BLOCKSIZE must be a multiple of 4. Blocks in the process of compression
 // can be any size from BLOCKSIZE to 3*BLOCKSIZE in steps of 4. TWISTPRIME is
 // the smallest prime greater than BLOCKSIZE, which is relatively prime to all
@@ -16,10 +17,10 @@ fn lfsr1(n:u32) -> u32 {
 
 lazy_static! {
   // RELPRIMES[0]=find_max_order(32), RELPRIMES[1]=find_max_order[36], etc.
-  static ref RELPRIMES: [u16; SZ as usize] = {
-    let mut m: [u16; SZ as usize]=[0;SZ as usize];
+  static ref RELPRIMES: [u16; SZ] = {
+    let mut m: [u16; SZ as usize]=[0;SZ];
     for i in 0..SZ {
-      m[i]=find_max_order(((BLOCKSIZE as usize)+4*i) as u64) as u16;
+      m[i]=find_max_order(((BLOCKSIZE)+4*i) as u64) as u16;
     }
     m
   };
@@ -39,6 +40,14 @@ lazy_static! {
 
 fn round_compress(sbox:&[[u8; 256]; 3], buf:&mut Vec<u8>, sboxalt:u32) {
   // The number of bytes in buf is decreased by 4 per round.
+  assert!(buf.len()&3==0);
   let mut tmp:Vec<u8> = Vec::new();
   tmp.extend_from_slice(buf);
+  let rprime=RELPRIMES[(buf.len()-BLOCKSIZE as usize)/4] as usize;
+  let len=buf.len()/3;
+  mix3parts(buf,len,rprime);
+  for i in 0..buf.len() {
+    buf[i]=sbox[((sboxalt as usize)+i)%3][buf[i] as usize];
+  }
+  rot_bitcount(buf, &mut tmp, TWISTPRIME);
 }
