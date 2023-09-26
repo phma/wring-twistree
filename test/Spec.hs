d2acaa5d27db3ae6d11c4cc78f0a77e446bcd5f4
Pierre Abbat <phma@bezitopo.org>
2023-09-26 07:52:49 +0000

    Add test vectors with empty key

--- test/Spec.hs
+++ test/Spec.hs
@@ -20,11 +20,12 @@ key6 = "aerate"
 wring96 = keyedWring $ fromString key96
 wring30 = keyedWring $ fromString key30
 wring6 = keyedWring $ fromString key6
+wring0 = keyedWring $ fromString ""
 
 main = defaultMain tests
 
 tests :: TestTree
-tests = testGroup "Tests" [properties, unitTests, testVectors]
+tests = testGroup "Tests" [properties, unitTests, testVectorsWring]
 
 properties :: TestTree
 properties = testGroup "Properties" [qcProps]
@@ -57,7 +58,7 @@ unitTests = testGroup "Unit tests"
       (mul65537 65535 65535) @?= 0
   ]
 
-testVectors = testGroup "Test vectors"
+testVectorsWring = testGroup "Test vectors for Wring"
   [ testCase "lin8nulls" $
       (encrypt linearWring (fromListN 8 [0,0,0,0,0,0,0,0]))
       @?= fromListN 8 [0x04,0xd7,0x16,0x6a,0xca,0x70,0x57,0xbc]
@@ -76,6 +77,24 @@ testVectors = testGroup "Test vectors"
   , testCase "lin9AllOrNone" $
       (encrypt linearWring (fromListN 9 $ stringToBytes "AllOrNone"))
       @?= fromListN 9 [0x53,0x28,0xe7,0xc7,0xe0,0x71,0xa5,0x2e,0x8c]
+  , testCase "null8nulls" $
+      (encrypt wring0 (fromListN 8 [0,0,0,0,0,0,0,0]))
+      @?= fromListN 8 [0x77,0x3e,0x34,0x8f,0x48,0xa1,0x24,0x1a]
+  , testCase "null8ff" $
+      (encrypt wring0 (fromListN 8 [255,255,255,255,255,255,255,255]))
+      @?= fromListN 8 [0xc7,0xa7,0x58,0xed,0x5c,0x2b,0xb6,0xec]
+  , testCase "null8Twistree" $
+      (encrypt wring0 (fromListN 8 $ stringToBytes "Twistree"))
+      @?= fromListN 8 [0xa3,0xcf,0xd4,0xa1,0x0d,0x7e,0xb7,0xb3]
+  , testCase "null9nulls" $
+      (encrypt wring0 (fromListN 9 [0,0,0,0,0,0,0,0,0]))
+      @?= fromListN 9 [0x10,0x10,0x95,0x96,0x90,0xb5,0x97,0xeb,0x38]
+  , testCase "null9ff" $
+      (encrypt wring0 (fromListN 9 [255,255,255,255,255,255,255,255,255]))
+      @?= fromListN 9 [0x09,0x0f,0xf3,0x66,0x36,0xa4,0xac,0x8d,0x5c]
+  , testCase "null9AllOrNone" $
+      (encrypt wring0 (fromListN 9 $ stringToBytes "AllOrNone"))
+      @?= fromListN 9 [0xee,0x15,0x02,0x05,0xdd,0xa9,0x77,0xe4,0x23]
   , testCase "aerate8nulls" $
       (encrypt wring6 (fromListN 8 [0,0,0,0,0,0,0,0]))
       @?= fromListN 8 [0x23,0x44,0x2e,0x6e,0xf3,0xd7,0xa0,0x7e]
